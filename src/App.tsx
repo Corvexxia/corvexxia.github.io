19e9af3683643ce98f0a99630272c07a8ea59b54
corvexxia <corvexxia@gmail.com>
2025-06-11 08:34:47 -0700

    Tried fixing routing

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
     <Router>
       <NavBar />
       <Routes>
-        <Route path="" element={<HomePage />} />
-        <Route path="projects" element={<ProjectsPage />} />
+        <Route path="https://corvexxia.github.io" element={<HomePage />} />
+        <Route path="https://corvexxia.github.io/projects" element={<ProjectsPage />} />
       </Routes>
     </Router>
       
